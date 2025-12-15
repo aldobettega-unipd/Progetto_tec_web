c234fcb691fdc63deb5d81804b1dcc5fc799bc65
aldobettega <aldo.bettega04@gmail.com>
2025-12-15 20:02:42 +0100

    creato db

--- init.sql
+++ init.sql
@@ -1,8 +1,31 @@
-CREATE TABLE IF NOT EXISTS contatti (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    data_invio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE utente (
+    username VARCHAR(20) PRIMARY KEY,
+    hash_password VARCHAR(32) NOT NULL
 );
 
-INSERT INTO contatti (nome, email) VALUES ('Test Utente', 'test@example.com');
+CREATE TABLE artista (
+    nome VARCHAR(50) PRIMARY KEY,
+    descrizione TEXT NOT NULL
+);
+
+CREATE TABLE canzone (
+    id VARCHAR(3) PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    artista VARCHAR(50) NOT NULL REFERENCES artista(nome),
+    descrizione TEXT NOT NULL,
+    testo_canzone TEXT NOT NULL
+);
+
+CREATE TABLE playlist (
+    id VARCHAR(3) PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    username VARCHAR(20) NOT NULL REFERENCES utente(username)
+);
+
+CREATE TABLE canzoni_playlist (
+    playlist VARCHAR(3) REFERENCES playlist(id),
+    canzone VARCHAR(3) REFERENCES canzone(id),
+    PRIMARY KEY(playlist, canzone)
+);
+
+

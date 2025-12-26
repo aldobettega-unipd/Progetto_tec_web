c5440c96476ca02ac76229d6231ea1a8e5dd242c
aldobettega <aldo.bettega04@gmail.com>
2025-12-26 22:21:15 +0100

    Feat: implementata una ricerca dalla home funzionante, ricerca artisti e canzoni

---
+++ insert.sql
@@ -0,0 +1,31 @@
+INSERT INTO
+    artista (nome, descrizione)
+VALUES
+    (
+        "Zucchero",
+        "Zucchero Fornaciari uno dei pi importanti cantautori italiani. Ha unito blues, rock e soul alla tradizione melodica italiana, creando uno stile riconoscibile. Artista internazionale, ha collaborato con grandi musicisti e portato la musica italiana sui palchi di tutto il mondo con successo, passione e una voce inconfondibile potente elegante"
+    ),
+    (
+        "Red Hot Chili Peppers",
+        "I Red Hot Chili Peppers sono leggende indiscusse del funk-rock californiano. Nati a Los Angeles, fondono l'energia del punk con groove funky e melodie alternative. Guidati dal carisma di Anthony Kiedis e dal basso virtuoso di Flea, hanno segnato la storia della musica con album iconici come Blood Sugar Sex Magik e Californication"
+    )
+;
+
+INSERT INTO
+    canzone (id, nome, artista, descrizione, testo_canzone)
+VALUES
+    (
+        "001",
+        "Un diavolo in me",
+        "Zucchero",
+        "Uno dei brani più famosi di Zucchero, contenuto nell'album Blue's.",
+        "… I've got a message for you From my heavenly father Everybody out there united for peace, love, joy, happiness Let it in your soul this morning, brothers and sisters Let him come on out, let him come on in Everybody knows he's there, come on and listen to me Come on brothers and sisters Sanctify, sanctify, sanctify your soul, sanctify, amen … I've got the devil in me … Gloria nell'alto dei cieli Ma non c'è pace quaggiù Non ho bisogno di veli Sei già un angelo tu Che accendi un diavolo in me Accendi un diavolo in me Perché c'è un divaolo in me Forse c'è un diavolo in me … Ooh, your eyes hypnotize me You're lips took control You put your arms around me And you shook my soul Every night I thought about what could I do Baby I find myself in love with you, oh … I've got the devil in me Perché c'è un diavolo in me, babe Forse c'è un diavolo in me … Saluta i tuoi (O-U) E bacia i miei (B-L) Che sensazione (E) Yeah … You take my love (O-U) Turn me around (B-L-E) Put my feet … Dai che non siamo dei santi Oh what you're trying to do Le tentazioni del suolo What you're trying to put me through Sono cose piccanti Release me Belle da prendere al volo E accendi un diavolo in me I got the devil in me Perché c'è un diavolo in me, babe Everybody can see I've got the devil, go Loose here, loose here Oh Break that, break that spell … You made me cry (B-L) (E) You told me a lie … Gave me pain (B-L-E) From the very start I've got the devil in me … Gloria nell'alto dei cieli Ma non c'è pace quaggiù (Help me somebody) È una questione di peli Sei proprio un angelo tu Che accendi un diavolo in me … I've got the devil in me Perché c'è un diavolo in me, babe I've got the devil in me Accendi un diavolo in me I've got the devil, devil Accendi un diavolo in me Devil devil devil in me Perché c'è un diavolo in me Can you see, can you see? Forse c'è un diavolo in me Can you tell, can you tell? Let's go Release me Devil Set me free I've got, ooh, devil in me"
+    ),
+    (
+        "002",
+        "Can't stop",
+        "Red Hot Chili Peppers",
+        "Singolo iconico del 2002 caratterizzato dal celebre riff di John Frusciante.",
+        "Can't stop, addicted to the shindig Chop Top, he says I'm gonna win big Choose not a life of imitation Distant cousin to the reservation Defunkt, the pistol that you pay for This punk, the feelin' that you stay for In time, I want to be your best friend East Side love is living on the West End Knocked out, but boy, you better come to Don't die, you know, the truth is some do Go write your message on the pavement Burn so bright, I wonder what the wave meant White heat is screamin' in the jungle Complete the motion if you stumble Go ask the dust for any answers Come back strong with fifty belly dancers The world I love, the tears I drop To be part of the wave, can't stop Ever wonder if it's all for you? The world I love, the trains I hop To be part of the wave, can't stop Come and tell me when it's time to Sweetheart is bleeding in the snow cone So smart, she's leading me to ozone Music, the great communicator Use two sticks to make it in the nature I'll get you into penetration The gender of a generation The birth of every other nation Worth your weight, the gold of meditation This chapter's gonna be a close one Smoke rings, I know you're gonna blow one All on a spaceship, persevering Use my hands for everything but steering Can't stop the spirits when they need you Mop tops are happy when they feed you J. Butterfly is in the treetop Birds that blow the meaning into bebop The world I love, the tears I drop To be part of the wave, can't stop Ever wonder if it's all for you? The world I love, the trains I hop To be part of the wave, can't stop Come and tell me when it's time to Wait a minute, I'm passin' out, win or lose Just like you Far more shocking than anything I ever knew How about you? Ten more reasons why I need somebody new Just like you Far more shocking than anything I ever knew Right on cue Can't stop, addicted to the shindig Chop Top, he says I'm gonna win big Choose not a life of imitation Distant cousin to the reservation Defunkt, the pistol that you pay for This punk, the feelin' that you stay for In time, I want to be your best friend East Side love is living on the West End Knocked out, but boy, you better come to Don't die, you know, the truth is some do Go write your message on the pavement Burn so bright, I wonder what the wave meant Kick-start the golden generator Sweet talk but don't intimidate her Can't stop the gods from engineering Feel no need for any interfering Your image in the dictionary This life is more than ordinary Can I get two, maybe even three of these? Comin' from space to teach you of the Pleiades Can't stop the spirits when they need you This life is more than just a read-through"
+    )
+;

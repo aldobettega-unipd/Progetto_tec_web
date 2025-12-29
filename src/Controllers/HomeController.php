2e1a74184f43a34dd2e6d76a70999717e4d72586
aldobettega <aldo.bettega04@gmail.com>
2025-12-29 12:45:19 +0100

    Fix: eliminato model Home inutile

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__ . '/../../config.php';
-require_once __DIR__ . '/../Models/Home.php';
 
 class HomeController {
     private $db;
--- src/Models/Home.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Home {
-    private $db;
-
-    public function __construct($db) {
-        $this->db = $db;
-    }
-}

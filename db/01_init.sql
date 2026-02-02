27c5e9fb19d09904361d80018e1d7cae81b16a45
aldobettega <aldo.bettega04@gmail.com>
2026-02-02 17:55:29 +0100

    Aggiornato init.db

--- db/01_init.sql
+++ db/01_init.sql
@@ -16,7 +16,8 @@ CREATE TABLE artista (
     id_utente INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nome_artista VARCHAR(50) NOT NULL UNIQUE,
     descrizione_artista TEXT NOT NULL,
-    slug_artista VARCHAR(30) NOT NULL
+    slug_artista VARCHAR(30) NOT NULL,
+    lingua_artista VARCHAR(3) DEFAULT NULL
 );
 
 CREATE TABLE canzone (

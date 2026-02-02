ca70f0cab9b14c17dc02bdd2936421aa74d8534c
CCaco4 <sanguinmarco07@gmail.com>
2026-02-02 23:08:34 +0100

    config

--- config.php
+++ config.php
@@ -7,4 +7,4 @@ define('DB_USER', getenv('DB_USER') ?: 'msanguin');
 define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'thooYeefu6aiveih');
 define('DB_NAME', getenv('DB_NAME') ?: 'msanguin');
 define('DB_CHAR', 'utf8mb4');
-define('BASE_URL','/msanguin');
+define('BASE_URL','');
